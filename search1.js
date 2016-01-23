d0b97426e56b623fd14bb6ad28b1c83658d6a163
Shaw <cshaw55@gmail.com>
2016-01-23 10:52:44 -0500

    Create search1.js
    Native JSON data set, search script

---
+++ search1.js
@@ -0,0 +1,31 @@
+var json = {
+    "movie": [{
+            "title": "Rising Sun",
+            "actor": "Sean Connery",
+            "minutes": 160},
+        {
+            "title": "Entrapment",
+            "actor": "Sean Connery",
+            "minutes": 130},
+        {
+            "address": "The Matrix",
+            "actor": "Keanu Reeves",
+            "minutes": 130}]
+
+};
+
+var movies = json["movie"];
+for(var i=0; i < movies.length; ++i) {
+    var movies_i = movies[i];
+    if(movies_i["title"] == "Rising Sun") {
+        
+        document.write(movies_i.title + "<br>");
+        //break;
+        }
+        else {
+        document.write('0 Results'+"<br>");
+        //break;
+       }
+        
+    
+}
