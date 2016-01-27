912f14b6e4fadc115fb9ce69bcf19d58f3cabb83
Shaw <cshaw55@gmail.com>
2016-01-26 23:35:37 -0500

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,14 +1,18 @@
 movies =
   [
     {"movie" : { "title" : "The Matrix Reloaded", "actor" : "Keanu Reeves" }},
-    {"movie" : { "title" : "Fight Club",        "actor" : "Edward Norton" }},
-    {"movie" : { "title" : "Strange Days",  "actor" : "Ralph Fiennes" }}
+    {"movie" : { "title" : "Fight Club","actor" : "Edward Norton","releasedate":"2001", "budget":"40 Million USD" }},
+    {"movie" : { "title" : "Strange Days","actor" : "Ralph Fiennes" }}
   ];
  
 for(var i = 0; i < movies.length; i++)
 {
-  if(movies[i].movie.actor == 'Edward Norton')
+  if(movies[i].movie.title == 'Fight Club')
   {
-    document.write(movies[i].movie.title)
+    document.write(movies[i].movie.title +"<br/>")
+    document.write(movies[i].movie.actor+"<br/>")
+    document.write(movies[i].movie.releasedate+"<br/>")
+    document.write(movies[i].movie.budget+"<br/>")
+    
   }
 }
