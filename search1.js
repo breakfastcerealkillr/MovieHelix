8b71133a278d5f7e9b763a0584f634ca7b9b5b83
Shaw <cshaw55@gmail.com>
2016-01-28 23:43:46 -0500

    Update search1.js

--- search1.js
+++ search1.js
@@ -29,3 +29,26 @@ for(var i=0; i < movies.length; ++i) {
         
     
 }
+
+//another search method
+
+movies =
+  [
+    {"movie" : { "title" : "The Matrix Reloaded", "actor" : "Keanu Reeves" }},
+    {"movie" : { "title" : "Fight Club",        "actor" : "Edward Norton","releasedate":"2001", "budget":"40 Million USD" }},
+    {"movie" : { "title" : "Strange Days",  "actor" : "Ralph Fiennes" }}
+  ];
+ 
+for(var i = 0; i < movies.length; i++)
+{
+  if(movies[i].movie.title == 'The Matrix Reloaded')
+  {
+    document.write(movies[i].movie.title +"<br/>")
+    document.write(movies[i].movie.actor+"<br/>")
+    document.write(movies[i].movie.releasedate+"<br/>")
+    document.write(movies[i].movie.budget+"<br/>")
+    
+  }
+}
+
+
