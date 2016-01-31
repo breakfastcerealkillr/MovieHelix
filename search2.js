dde599c29b809541ea039456bcb4fa42f1176e72
Shaw <cshaw55@gmail.com>
2016-01-31 00:38:15 -0500

    Update search2.js

--- search2.js
+++ search2.js
@@ -51,3 +51,24 @@ var found = $.map(myArray, function(val) {
 });
 
 alert(found.length + "\n" + found[0]);
+
+//more
+
+restaurants =
+  [
+    {"restaurant" : { "name" : "McDonald's", "food" : "burger" }},
+    {"restaurant" : { "name" : "KFC",        "food" : "chicken" }},
+    {"restaurant" : { "name" : "Pizza Hut",  "food" : "pizza" }}
+  ];
+ 
+for(var i = 0; i < restaurants.length; i++)
+{
+  if(restaurants[i].restaurant.food == 'pizza')
+  {
+    document.write(restaurants[i].restaurant.name)
+  }
+}
+
+
+
+
