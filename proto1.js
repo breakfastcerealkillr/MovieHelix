13f9e7e58285826ab21f8bb3629eaaedd5f38bee
Scott Conley <scott@flipstone.com>
2016-02-05 15:05:23 -0500

    cleanup

---
+++ proto1.js
@@ -0,0 +1,59 @@
+//DATASET HERE
+var movies =
+  [
+    { "title" : "a", "actor" : "Keanu Reeves", "releasedate":"1999", "budget":"90 Million USD"  },
+    { "title" : "b", "actor" : "Edward Norton", "releasedate":"2001", "budget":"40 Million USD" },
+    { "title" : "c", "actor" : "Ralph Fiennes", "releasedate":"1997", "budget":"60 Million USD"}
+  ];
+
+var titleInput1  = document.getElementById("title1");
+var titleInput2  = document.getElementById("title2");
+var log = document.getElementById("log");
+
+function lookupMovies ( ) {
+  clearLog();
+  var movie1 = false;
+  var movie2 = false;
+
+  // Loop through the movies and initialize our comparison targets
+  for (var i = 0; i < movies.length; i++)
+  {
+    if ( movies[i].title == titleInput1.value )
+    {
+      appendLog("Matched Movie 1: " + movies[i].title + ", starring " + movies[i].actor)
+      movie1 = movies[i];
+    }
+    if ( movies[i].title == titleInput2.value )
+    {
+      appendLog("Matched Movie 2: " + movies[i].title + ", starring " + movies[i].actor)
+      movie2 = movies[i];
+    }
+    if (movie1 && movie2) break;
+  }
+
+  // compare our matches
+  var set1 = indexMovie(movie1);
+  var set2 = indexMovie(movie2);
+}
+
+function indexMovie(movie) {
+  var set = [];
+  for(var key in movie) {
+    if (key != "title") {
+      set.push(movie[key]);
+    }
+  }
+  return set;
+}
+
+function clearLog () {
+  while( log.firstChild ){
+    log.removeChild( log.firstChild );
+  }
+}
+
+function appendLog (text) {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(text));
+  log.appendChild(li);
+}
